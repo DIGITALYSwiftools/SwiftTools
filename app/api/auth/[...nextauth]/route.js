151254640804afd10d920987ab3f073782543565
Iqra Malik <im4356927@gmail.com>
2026-01-17 14:12:43 +0500

    add next-auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,8 +2,8 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 
-import { connectDB } from "@/lib/db";
-import User from "@/lib/models/User";
+import { connectDB } from "../../../lib/db";
+import User from "../../../models/user";
 
 export const authOptions = {
   session: {
